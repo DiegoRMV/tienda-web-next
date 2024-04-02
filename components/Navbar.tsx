93afc78dbc424d035e7186fbd6f78e78f5fc8f7c
DiegoRMV <95139700+DiegoRMV@users.noreply.github.com>
2024-04-02 16:59:52 -0300

    prueba de commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 
 export default function Navbar() {
 	const [isClick, setIsClick] = useState(false);
+	
 	const toggleNavbar = (): void => {
 		setIsClick(!isClick);
 	};
